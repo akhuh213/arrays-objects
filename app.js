4f2e3e3ae00d835f12d46491d5c9b3711a3ebef7
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-15 17:51:19 -0400

    [family]array

--- app.js
+++ app.js
@@ -11,15 +11,15 @@ var teams = ['Rockets','Lakers','Warriors','Clippers','Blazers','Celtics','Nets'
 console.log(teams);
 
 // while loops
-var number = 10;
-var otherNumber = 1
-while (otherNumber < number) {
-    console.log(number);
+//var number = 10;
+//var otherNumber = 1
+//while (otherNumber < number) {
+   // console.log(number);
    // otherNumber += 1; 
-    otherNumber = otherNumber + 1; //
+    //number = otherNumber + 1; //
     //it says otherNumber = otherNumber + 1; will check the condition again and if the condition is true, 
     //will print again, and go back to the loop again till reach the number which is 10.
-}
+//}
 
 var numberTwo = 20;
 var myOtherNumber = 10;
@@ -28,9 +28,33 @@ while (myOtherNumber < numberTwo) {
     myOtherNumber += 1;
 }
 
+
+
+
 var myNumber = 1;
 var secondNumber = 10;
 while (secondNumber > myNumber) {
     console.log(myNumber);
     myNumber += 2;
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log(i); //0,1,2,3,4,5,6,7,8,9
+}
+
+// print teams 
+
+for (var i = 0; i < teams.length; i++) {
+    //one way consol.log(teams[i]) -> this will print all the elements in the array
+    var eachTeam = teams[i];
+    console.log(eachTeam);
+}
+
+
+
+var peopleILove = ['Sean','Joshua','Daniel','Yoon']
+
+for (var i = 0; i < peopleILove.length; i++) {
+    var eachPerson = peopleILove[i];
+    console.log(eachPerson);
 }
