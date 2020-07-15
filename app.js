9eaff82db607770fd3d631d2d3da61687150de75
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-15 17:03:47 -0400

    [arrays] Add favorite teams

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = 'Alice';
+console.log(name);
+
+var myLocation = 'Virginia'
+console.log(myLocation);
+
+var favoriteNumber = 6;
+console.log(favoriteNumber);
+
+var teams = ['Rockets','Lakers','Warriors','Clippers','Blazers','Celtics','Nets'];
+console.log(teams);
+
