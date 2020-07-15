c7a2464ed4aabe79f77c410e7a9092f004ce2fbc
Alice Aekyung Huh <aliceaekyunglee@alices-imac.myfiosgateway.com>
2020-07-15 17:22:32 -0400

    [loops] Add while loops

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ console.log(favoriteNumber);
 var teams = ['Rockets','Lakers','Warriors','Clippers','Blazers','Celtics','Nets'];
 console.log(teams);
 
+// while loops
+var number = 10;
+var otherNumber = 1
+while (otherNumber < number) {
+    console.log(number);
+   // otherNumber += 1; 
+    otherNumber = otherNumber +1; //
+    //it says otherNumber = otherNumber + 1; will check the condition again and if the condition is true, 
+    //will print again, and go back to the loop again till reach the number which is 10.
+}
+
+var numberTwo = 20;
+var myOtherNumber = 10;
+while (myOtherNumber < numberTwo) {
+    console.log(myOtherNumber);//10
+    myOtherNumber += 1;
+}
